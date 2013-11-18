0d7e5edeb48775de94fe55094554bda206004e2c
AcrA <skald2828@gmail.com>
2013-11-18 23:11:59 +0400

    Revert 71454f0..7ed629c
    This rolls back to commit 71454f0207d40569a6fe6d7254f71a22d7c5b4d8.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,26 @@
 #include <vector>
 using namespace std;
 
+struct Element {
+	char symbol;
+	vector<int> dependsOn;
+
+	void addDependsOn(int d) {
+		bool isHere = false;
+		for (int i = 0; i < dependsOn.size(); i++) {
+			if (dependsOn[i] == d) {
+				isHere = true;
+				break;
+			}
+		}
+		if (!isHere) {
+			dependsOn.resize(dependsOn.size() + 1);
+			dependsOn[dependsOn.size() - 1] = d;
+		}
+	}
+
+};
+
 struct Cortege {
 private:
 	string X;
